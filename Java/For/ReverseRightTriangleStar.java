83f0f8afd9a2a256afb8e63d2a08dc288322b324
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-03-19 18:20:29 +0900

    역직삭감형 별찍기

---
+++ Java/For/ReverseRightTriangleStar.java
@@ -0,0 +1,12 @@
+public class ReverseRightTriangleStar {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i < 6; i++) {
+            for (int j = i; j < 6; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

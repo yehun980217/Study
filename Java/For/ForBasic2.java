6610e21ca80dfba0e657d483f09b8f8e746a8e65
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-03-20 16:06:06 +0900

    for문 기초 공부 10까지의 숫자 더하기

---
+++ Java/For/ForBasic2.java
@@ -0,0 +1,9 @@
+public class ForBasic2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+            System.out.println("1부터 " + i + "까지의 합 : " + sum);
+        }
+    }
+}

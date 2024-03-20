a55a4c7a3fb3bdb46ea6bb792ec4adb6afb6034a
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-03-20 12:08:46 +0900

    printf() 함수의 서식 지정

---
+++ C/Printf()Function/printfForm2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main() {
+
+	printf("%d \n", 123);
+	printf("%5d \n", 123);
+	printf("%05d \n", 123);
+
+	printf("%f\n", 123.45);
+	printf("%7.1f\n", 123.45);
+	printf("%2.5f\n", 123.45);
+	printf("%7.3f\n\n", 123.45);
+
+	printf("%s\n", "Basic-C");
+	printf("%20s\n\n", "Basic-C");
+	printf("%3s\n\n", "Basic-C");
+}
+

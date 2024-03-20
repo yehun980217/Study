1e9a9de4a4a5a5d7037fffeebd93978abc7a34d1
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-03-20 16:06:29 +0900

    for문을 이용한 구구단 출력

---
+++ Java/For/GuguDan.java
@@ -0,0 +1,13 @@
+public class GuguDan {
+
+    public static void main(String[] args) {
+        // 구구단 출력하기
+        for(int i = 2; i < 10; i++) {
+            System.out.println(i + "단");
+            System.out.println("────────────────");
+            for(int j = 1; j < 10; j++) {
+                System.out.println(i + " X " + j + " = " + (i * j));
+            }
+        }
+    }
+}

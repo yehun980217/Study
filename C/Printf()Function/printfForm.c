cfe2be31eca1e152d68cefa72dc4e651f114546d
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-03-20 12:08:26 +0900

    printf() 함수의 서식 지정

---
+++ C/Printf()Function/printfForm.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main() {
+	printf("\n줄바꿈\n줄바꿈\n줄바꿈\n");
+	printf("\t탭키\t탭키\t탭키\n");
+	printf("이것을\r덮어씁니다\n");
+	printf("이것을\r덮어씁니다\r앙앙\n");
+	printf("\a삐소리 1번\n");
+	printf("글자가 \"강조\"되는 효과 \n");
+	printf("\\\\\\ 6개는 역슬래쉬 세개 출력 \n");
+}

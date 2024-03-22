1da069db6f20c0e03ea12c00b6e61ffd2777644e
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-03-22 20:28:00 +0900

    for문을 이용한 배열 출력

---
+++ Java/Array/ArrayExam2.java
@@ -0,0 +1,12 @@
+package javaStudy0317;
+
+public class Array {
+
+    public static void main(String[] args) {
+
+        // arr 배열 중에서 인덱스가 1인 곳의 값을 출력
+        int[] arr = {10, 20, 30, 40, 50}; // 인덱스값은 0부터 시작, 이 배열은 0~4의 인덱스를 가짐
+
+        System.out.println(arr[1]);
+    }
+}

0a7a37aac4267d13a1ddb7af9e548b1e673195ce
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-03-23 20:53:48 +0900

    if문 활용 연령별 버스 카드

--- Java/If/BusCard.java
+++ Java/If/BusCard.java
@@ -16,7 +16,7 @@ public class BusCard {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
-        String role = getRole(age);
+        String role = getRole(age); // getRole클래스에 사용자로부터 입력받은 age를 보냄
         System.out.println(role);
     }
 }

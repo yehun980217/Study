cc0faf5d344a6f88aa58221ba368b3a1056bd9d1
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-03-25 17:49:06 +0900

    특수기호 공부 2

---
+++ StringExam2.java
@@ -0,0 +1,16 @@
+public class StringExam2 {
+    public static void main(String[] args) {
+        System.out.println("\n줄바꿈\n연습");
+        System.out.println("\t탭키 \t연습");
+        System.out.println("어떤 글자를 \"강조\"하는 효과1");
+        System.out.println("어떤 글자를 '강조'하는 효과2");
+        System.out.println("\\\\ 백슬러시 2개 출력");
+
+        /*  줄바꿈
+            연습
+                탭키 	연습
+            어떤 글자를 "강조"하는 효과1
+            어떤 글자를 '강조'하는 효과2
+            \\ 백슬러시 2개 출력 */
+    }
+}

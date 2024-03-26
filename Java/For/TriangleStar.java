f501c4f6888be2d6b4c2859eb969d172f7ed590a
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-03-26 13:36:36 +0900

    정삼각형 별 찍기

--- Java/For/TriangleStar.java
+++ Java/For/TriangleStar.java
@@ -2,7 +2,7 @@ public class TriangleStar {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i < 5; i++) {
             if (i == 1) System.out.print("    ");
             else if (i == 2) System.out.print("   ");
             else if (i == 3) System.out.print("  ");
